80ede82f066e9a0fc04526d8dc9caae3a8bf4e17
Santiago Baldrich <santiago.baldrich@gmail.com>
2014-08-26 22:04:48 -0500

    Round 263 cleanup

--- Codeforces/Round 263 - Div 2 [3 | 5]/B - [Appleman and Card Game].cpp
+++ Codeforces/Round 263 - Div 2 [3 | 5]/B - [Appleman and Card Game].cpp
@@ -2,28 +2,11 @@
 
 using namespace std;
 
-#define D(x) cout << "[ " << #x << " => " << x << " ]" << endl;
-#define DD(x,y) cout << "[ " << #x << " => " << x << ", " << #y << " => " << y << " ]" << endl;
-#define DDD(x,y,z) cout << "[ " << #x << " => " << x << ", " << #y << " => " << y << ", " << #z << " => " << z << " ]" << endl;
-#define all(c) c.begin(),c.end()
 #define tr(c, it) for(typeof(c.begin()) it = c.begin(); it != c.end(); it++)
-
-template<class T>
-inline ostream& operator<<(ostream& out, vector<T> v){
-	out << "[";
-	int i,n = v.size();
-	for(i=0; i<n; i++)
-		out << " " << v[i];
-	return out << " ]";
-}
-
-template<class S, class T>
-inline ostream& operator<<(ostream& out, pair<S,T> pp){
-	return out << "{ " << pp.first << ", " << pp.second << " }";
-}
 
 typedef pair<int,char> ic;
 typedef long long ll;
+
 int main(){
 	ios :: sync_with_stdio( 0 );
 	cin.tie( 0 );
--- Codeforces/Round 263 - Div 2 [3 | 5]/C - [Appleman and Toastman].cpp
+++ Codeforces/Round 263 - Div 2 [3 | 5]/C - [Appleman and Toastman].cpp
@@ -2,27 +2,7 @@
 
 using namespace std;
 
-#define D(x) cout << "[ " << #x << " => " << x << " ]" << endl;
-#define DD(x,y) cout << "[ " << #x << " => " << x << ", " << #y << " => " << y << " ]" << endl;
-#define DDD(x,y,z) cout << "[ " << #x << " => " << x << ", " << #y << " => " << y << ", " << #z << " => " << z << " ]" << endl;
 #define all(c) c.begin(),c.end()
-#define tr(c, it) for(typeof(c.begin()) it = c.begin(); it != c.end(); it++)
-#define max(a,b) (a) > (b) ? (a) : (b) 
-#define min(a,b) (a) < (b) ? (a) : (b) 
-
-template<class T>
-inline ostream& operator<<(ostream& out, vector<T> v){
-	out << "[";
-	int i,n = v.size();
-	for(i=0; i<n; i++)
-		out << " " << v[i];
-	return out << " ]";
-}
-
-template<class S, class T>
-inline ostream& operator<<(ostream& out, pair<S,T> pp){
-	return out << "{ " << pp.first << ", " << pp.second << " }";
-}
 
 typedef long long ll;
 
